45d0c634aa5c7f40842a54220d3d3f4e6aaf4e52
Dan Folescu <dan1345@gmail.com>
2024-10-14 10:13:22 -0400

    AMDGPU free bytes api updated for julia 1.11+

--- ext/AMDGPUPseudospectra.jl
+++ ext/AMDGPUPseudospectra.jl
@@ -9,7 +9,7 @@ if AMDGPU.functional()
         KAPseudospectra.device!(B::AMDGPU.ROCBackend, dev) = AMDGPU.device!(dev)
         KAPseudospectra.devices(B::AMDGPU.ROCBackend) = AMDGPU.devices()
         KAPseudospectra.get_bgarray(B::AMDGPU.ROCBackend) = AMDGPU.ROCArray
-        KAPseudospectra.device_bytes_available(B::AMDGPU.ROCBackend) = AMDGPU.Runtime.Mem.free()
+        KAPseudospectra.device_bytes_available(B::AMDGPU.ROCBackend) = AMDGPU.free()
         KAPseudospectra.device_reclaim(B::AMDGPU.ROCBackend) = AMDGPU.HIP.reclaim()
     end
 
